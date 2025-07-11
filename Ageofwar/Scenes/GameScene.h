2f0ba94f821523c17fdfec5211ede0e3f37aaf8d
이재영 <kltop1210@gmail.com>
2025-07-11 18:02:36 +0900

    GameScene 필요없는 구문 제거중

--- Ageofwar/Scenes/GameScene.h
+++ Ageofwar/Scenes/GameScene.h
@@ -6,28 +6,23 @@ class TextGo;
 class GameScene : public Scene
 {
 protected:
-	enum class CollisionMode
-	{
-		Rectangle,
-		Circle
-	};
+	sf::Sprite background;
+	
 
-	sf::RectangleShape* rect1;
-	sf::RectangleShape* rect2;
-	sf::CircleShape* circle1;
-	sf::CircleShape* circle2;
+	
 	TextGo* collisionText;
 	TextGo* modeText;
 	
-	CollisionMode currentMode = CollisionMode::Rectangle;
-	bool isColliding = false;
+
+	
 
 public:
-	GameScene();
-	~GameScene() override;
+	GameScene();	
+	~GameScene() = default;
 
 	void Init() override;
 	void Enter() override;
+
 	void Update(float dt) override;
 	void Draw(sf::RenderWindow& window) override;
 };
